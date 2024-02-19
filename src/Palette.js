fe24a2a4feb7c5b7003df7256e29994a87652366
khabibullo <saidakhmatov.k@gmail.com>
2024-02-19 15:36:24 +0500

    created react app, added palette

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      {/* Footer eventually */}
+    </div>
+  );
+};
+
+export default Palette;
