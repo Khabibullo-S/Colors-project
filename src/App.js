b92ac217b60be8d27f11c3a4ab4c913961832ad9
khabibullo <saidakhmatov.k@gmail.com>
2024-02-20 22:30:28 +0500

    Fix Palette Route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+
+function PaletteWrapper() {
+  const { id } = useParams(); // Now useParams has the context it needs
+  if (!seedColors.find((p) => p.id === id)) {
+    return (
+      <div>
+        <h1>PALETTE NOT FOUND!</h1>
+        <p>palette example: /palette/flat-ui-colors-aussie</p>
+      </div>
+    );
+  }
+  const palette = generatePalette(seedColors.find((p) => p.id === id));
+  return <Palette palette={palette} />;
+}
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
-        <Route
-          exact
-          path="/palette/:id"
-          element={<h1>INDIVIDUAL PALETTE</h1>}
-        />
+        <Route path="/" exact element={<h1>PALETTE LIST GOES HERE</h1>} />
+        <Route path="/palette/:id" exact element={<PaletteWrapper />} />
       </Routes>
 
       {/* <Palette palette={generatePalette(seedColors[4])} /> */}
