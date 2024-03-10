509c4136761d54989a095212d5d55838f9bbdd30
khabibullo <saidakhmatov.k@gmail.com>
2024-03-10 12:28:41 +0500

    Animate Palette deletion

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,6 +2,10 @@ import React, { useMemo } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
 import getPaletteListStyles from "./styles/PaletteListStyles";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "@emotion/styled";
+
+const TRANSITION_TIME = 500;
 
 const PaletteList = ({ palettes, deletePalette }) => {
   const navigate = useNavigate();
@@ -11,6 +15,20 @@ const PaletteList = ({ palettes, deletePalette }) => {
     () => getPaletteListStyles(),
     []
   );
+  const TransitionGroupStyled = useMemo(
+    () => styled(TransitionGroup)`
+      ${PalettesDiv.__emotion_styles} // Apply the PalettesDiv styles here
+      .fade-exit {
+        opacity: 1;
+      }
+      .fade-exit-active {
+        opacity: 0;
+        transition: opacity ${TRANSITION_TIME}ms ease-out;
+      }
+    `,
+    []
+  );
+
   /* END OF STYLES */
   return (
     <RootDiv>
@@ -19,17 +37,25 @@ const PaletteList = ({ palettes, deletePalette }) => {
           <h1>React Colors</h1>
           <Link to="/palette/new">Create Palette</Link>
         </Nav>
-        <PalettesDiv>
+        {/* <PalettesDiv> */}
+        <TransitionGroupStyled>
           {palettes.map((palette) => (
-            <div onClick={() => navigate(`/palette/${palette.id}`)}>
-              <MiniPalette
-                {...palette}
-                deletePalette={deletePalette}
-                key={palette.id}
-              />
-            </div>
+            <CSSTransition
+              key={palette.id}
+              classNames="fade"
+              timeout={TRANSITION_TIME}
+            >
+              <div onClick={() => navigate(`/palette/${palette.id}`)}>
+                <MiniPalette
+                  {...palette}
+                  deletePalette={deletePalette}
+                  key={palette.id}
+                />
+              </div>
+            </CSSTransition>
           ))}
-        </PalettesDiv>
+        </TransitionGroupStyled>
+        {/* </PalettesDiv> */}
       </ContainerDiv>
     </RootDiv>
   );
